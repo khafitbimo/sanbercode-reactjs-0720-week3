b31356355d0fb83ec75732e7fb24e4fbc1581edc
khafitbimo-pc <khafit.bimo@gmail.com>
2020-08-04 15:19:45 +0700

    commit tugas 12 04/08/2020

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import HargaBuah from './tugas11/HargaBuah';
+import CountDown from './tugas12/CountDown'
 
-function App() {
-  return (
-    <div>
-      <HargaBuah />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 10,
+      showComponent12: true
+    }
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+        ()=>this.tick(),
+        1000
+    )
+
+    
+  }
+
+  tick(){
+    this.setState({
+        date: new Date(),
+        count: this.state.count - 1
+    })
+    if (this.state.count===0) {
+      this.setState({
+        showComponent12: !this.state.showComponent12
+      })
+      clearInterval(this.timerID)
+    }
+    
+  }
+
+
+  render(){
+
+    return (
+      <div>
+        
+        <HargaBuah />
+        {this.state.showComponent12===true ? <CountDown countStart={this.state.count}/> : null}
+       
+      </div>
+      
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/tugas12/CountDown.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+
+class CountDown extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            date : new Date(),
+            count : this.props.countStart
+        }
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(
+            ()=>this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+    tick(){
+        this.setState({
+            date: new Date(),
+            count: this.state.count - 1
+        })
+
+    }
+
+    render(){
+        
+        return(
+            <div className="container-tugas11">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><h2 className="label-left">sekarang jam : {this.state.date.toLocaleTimeString()}</h2></td>
+                            <td><h2 className="label-right">hitung mundur : {this.state.count}</h2></td>
+                        </tr>
+                    </tbody>
+                </table>
+               
+           </div>
+            
+        )
+    }
+}
+
+export default CountDown
